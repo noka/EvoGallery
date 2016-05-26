9ae439a898bc15ca0422fe4eb5770106f2ae755d
noka <nishioka@pan-publicity.co.jp>
2016-05-26 21:43:23 +0900

    mmのウィジェット呼び出し時のレイアウト調整

--- assets/plugins/managermanager/widgets/evogallery/evogallery.php
+++ assets/plugins/managermanager/widgets/evogallery/evogallery.php
@@ -23,11 +23,11 @@ function mm_widget_evogallery($moduleid, $title='', $roles='', $templates='') {
 	
 		//TODO: Add iframe autoheight
 		if (isset($content['id']))
-			$iframecontent = '<iframe id="mm_evogallery" src="'.$modx->config['site_url'].'manager/index.php?a=112&id='.$moduleid.'&onlygallery=1&action=view&content_id='.$content['id'].'" style="width:100%;height:600px;" scrolling="auto" frameborder="0"></iframe>';
+			$iframecontent = '<iframe id="mm_evogallery" src="'.$modx->config['site_url'].'manager/index.php?a=112&id='.$moduleid.'&onlygallery=1&action=view&content_id='.$content['id'].'" style="width:100%;min-height:600px;" scrolling="auto" frameborder="0"></iframe>';
 		else
 			$iframecontent = '<p class="warning">'.$_lang['mm_save_required'].'</p>';
 		
-		mm_createTab($title, 'evogallery', '', '', '<strong>'.$_lang['mm_manage_images'].'</strong>', '100%');
+		mm_createTab($title, 'evogallery', '', '', '<h3>'.$_lang['mm_manage_images'].'</h3>', '100%');
 		$output = "\$j('#table-evogallery').append('<tr><td>$iframecontent</td></tr>');";
 		
 	} // end if
